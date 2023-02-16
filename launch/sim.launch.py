85629e0157acb3ffde7ceb7c35a43714a1ecaece
Ralph Stirling <ralph.stirling@wallawalla.edu>
2023-02-16 15:00:51 -0800

    full simulation version of rosbot

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -34,8 +34,7 @@ def generate_launch_description():
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'rosbot',
-                                   'small_office.world'],
+                                   '-entity', 'rosbot'],
                         output='screen')
 
 
@@ -44,6 +43,6 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity,
+        spawn_entity
     ])
 
